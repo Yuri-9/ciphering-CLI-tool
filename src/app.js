4a2b99a0e64d25e98985431742dd58000ff1cffd
Vashkevich Yuri <vashkevichyp@gmail.com>
2021-11-10 23:35:03 +0300

    refactor: add class Streams

--- src/app.js
+++ src/app.js
@@ -1,18 +1,10 @@
-const fs = require('fs');
-const { pipeline } = require('stream');
 const errorHandler = require('./modules/customerError/errorHandler');
 const validateOptionCLI = require('./modules/validate/validateOptionCLI');
 const validateConfigCipher = require('./modules/validate/validateConfigCipher');
 const parseCLI = require('./modules/parseCLI/parseCLI');
-const CaesarCipherStream = require('./modules/cipher/CaesarCipherStream');
-const ROT8CipherStream = require('./modules/cipher/ROT8CipherStream');
-const AtbashCipherStream = require('./modules/cipher/AtbashCipherStream');
-const { CHAR_CAESAR, CHAR_ROT8, CHAR_ATBASH, SHIFT_CAESAR, SHIFT_ROT8 } = require('./const');
+const Streams = require('./modules/Streams/Streams');
 
 const appArgs = process.argv.slice(2);
-
-// const config = 'C1-R1-A';
-
 const app = async () => {
   try {
     await validateOptionCLI(appArgs);
@@ -20,32 +12,8 @@ const app = async () => {
     console.log(config, input, output);
     await validateConfigCipher(config);
 
-    const transformStreams = config.split('-').map((item) => {
-      switch (item[0]) {
-        case CHAR_CAESAR: {
-          const shift = +item[1] ? SHIFT_CAESAR : -SHIFT_CAESAR;
-          return new CaesarCipherStream({ highWaterMark: 10 }, shift);
-        }
-        case CHAR_ROT8: {
-          const shift = +item[1] ? SHIFT_ROT8 : -SHIFT_ROT8;
-          return new ROT8CipherStream({ highWaterMark: 10 }, shift);
-        }
-        case CHAR_ATBASH: {
-          return new AtbashCipherStream({ highWaterMark: 10 });
-        }
-        default: {
-          return new CaesarCipherStream({ highWaterMark: 10 }, 0);
-        }
-      }
-    });
-
-    const steams = [
-      fs.createReadStream('./src/input.txt', { encoding: 'utf8', highWaterMark: 10 }),
-      ...transformStreams,
-      fs.createWriteStream('./src/output.txt', { encoding: 'utf8' }),
-    ];
-
-    pipeline(steams, () => {});
+    const streams = new Streams(config, input, output);
+    streams.runPipeline();
   } catch (error) {
     errorHandler(error);
   }
---
+++ src/modules/Streams/Streams.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const { pipeline } = require('stream');
+const CaesarCipherStream = require('../cipher/CaesarCipherStream');
+const ROT8CipherStream = require('../cipher/ROT8CipherStream');
+const AtbashCipherStream = require('../cipher/AtbashCipherStream');
+const { CHAR_CAESAR, CHAR_ROT8, CHAR_ATBASH, SHIFT_CAESAR, SHIFT_ROT8 } = require('../../const');
+
+class Streams {
+  constructor(config, inputFile, outputFile) {
+    this.config = config;
+    this.inputFile = inputFile;
+    this.outputFile = outputFile;
+    this.readStream = fs.createReadStream(this.inputFile, {
+      encoding: 'utf8',
+      highWaterMark: 10,
+    });
+    this.writeStream = fs.createWriteStream(this.outputFile, {
+      encoding: 'utf8',
+      flags: 'a',
+    });
+  }
+
+  createChainTransformStreams() {
+    const transformStreams = this.config.split('-').map((item) => {
+      switch (item[0]) {
+        case CHAR_CAESAR: {
+          const shift = +item[1] ? SHIFT_CAESAR : -SHIFT_CAESAR;
+          return new CaesarCipherStream({ highWaterMark: 10 }, shift);
+        }
+        case CHAR_ROT8: {
+          const shift = +item[1] ? SHIFT_ROT8 : -SHIFT_ROT8;
+          return new ROT8CipherStream({ highWaterMark: 10 }, shift);
+        }
+        case CHAR_ATBASH: {
+          return new AtbashCipherStream({ highWaterMark: 10 });
+        }
+        default: {
+          return new CaesarCipherStream({ highWaterMark: 10 }, 0);
+        }
+      }
+    });
+    return transformStreams;
+  }
+
+  async runPipeline() {
+    const pipeline2 = new Promise((res, rej) => {
+      pipeline(
+        [this.readStream, ...this.createChainTransformStreams(), this.writeStream],
+        (err) => {
+          if (err) {
+            rej(err);
+          }
+          res();
+        }
+      );
+    });
+    await pipeline2;
+    fs.appendFile('./src/output.txt', '\n', () => {});
+  }
+}
+
+module.exports = Streams;
